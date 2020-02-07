c14fa1431f05cc59715912dd5a5f220b7d0a032a
Mara <mara-li@outlook.fr>
2020-02-07 03:33:36 +0100

    Réparation de bogue - Optimisation

--- CDI_dice.py
+++ CDI_dice.py
@@ -32,7 +32,10 @@ def checkError() :
             or defe_field.get() == "" or shield_field.get() == ""
             or d_endu_field.get() == "" or bonus_field.get() == ""
             or val_endu_field.get() == ""
-            or int(atq_field.get())>10 or int(defe_field.get())>10 or int(d_endu_field.get())>10 or int(val_endu_field.get())>10):
+            or int(atq_field.get())>10 or int(defe_field.get())>10 or int(d_endu_field.get())>10 or int(val_endu_field.get())>10)\
+            or int(pv_field.get())<=int(shield_field.get())\
+            or int(bonus_field.get())>100\
+            or int(pv_field.get()==0):
         # show the error message
         messagebox.showerror("Erreur","Il y a un problème dans les valeurs rentrées !")
 
@@ -45,6 +48,8 @@ def calculate_degat():
     if value== -1:
         return
     else:
+        cuirasse=0
+        encaissement=0
         pv=int(pv_field.get())
         atq=int(atq_field.get())
         defe=int(defe_field.get())
@@ -54,30 +59,57 @@ def calculate_degat():
         bonus=int(bonus_field.get())/100
 
         #Calcul des dégâts
-        d= abs(atq - defe)
-        if (d==0):
+        if (atq==0): #UltraCC de PJ
+            #Un ultra CC outrepasse TOUTES les défense de l'adversaire, cuirasse et défense compris.
+            d=0.5+bonus
+            encaissement = abs(int(d * pv))
+        elif (defe==0):
             d=0+bonus
-        elif (d==1):
-            d=0.5+bonus
-        elif (d==2):
-            d=0.1+bonus
-        elif (d==4 or d==3):
-            d=0.2+bonus
-        elif (d==5 or d==6):
-            d=0.3+bonus
-        elif (d==7 or d==8):
-            d=0.4+bonus
-        elif (d>=9):
-            d=0.5+bonus
 
-        d=abs(int(d*pv))
-        cuirasse=abs(int(d*(1-shield))) #Calcul des dégâts avec prise en compte de la cuirasse
+        elif (atq==1): #CC de Mob/Pj normaux
+            #Ici, on outrepasse la défense de l'adversaire, mais on garde tout de même la cuirasse.
+            d=0.5+bonus
+            d=abs(int(d * pv))
+            cuirasse = abs(int(d * (1 - shield)))  # Calcul des dégâts avec prise en compte de la cuirasse
 
-        #Verif réussite endurance
-        if (endu_de > endu_val):
-            encaissement=0
+            # Verif réussite endurance
+            if (endu_de > endu_val) :
+                encaissement = cuirasse
+            elif (endu_val == 0) :
+                encaissement = cuirasse
+            else :
+                encaissement = cuirasse * (1 - (10 * (abs(endu_val - endu_de) + 1)) / 100)
+                encaissement = int(encaissement)
+
         else:
-            encaissement = cuirasse * (1 - (10 * (abs(endu_val - endu_de) + 1)) / 100)
+            d= abs(atq - defe)
+            if (d==0):
+                de=0+bonus
+            elif (d==1):
+                de=0.05+bonus
+            elif (d==2):
+                de=0.1+bonus
+            elif (d==4 or d==3):
+                de=0.2+bonus
+            elif (d==5 or d==6):
+                de=0.3+bonus
+            elif (d==7 or d==8):
+                de=0.4+bonus
+            elif (d>=9):
+                de=0.5+bonus
+            d=abs(int(de*pv))
+            cuirasse=abs(int(d*(1-shield))) #Calcul des dégâts avec prise en compte de la cuirasse
+
+            #Verif réussite endurance
+            if (endu_de > endu_val):
+                encaissement=cuirasse
+            elif (endu_val==0):
+                encaissement=cuirasse
+            else:
+                encaissement = cuirasse * (1 - (10 * (abs(endu_val - endu_de) + 1)) / 100)
+        if (encaissement >= pv):
+            encaissement="Overkill"
+        else:
             encaissement=int(encaissement)
 
         #insert methode : value in the text entry box
@@ -93,7 +125,10 @@ if __name__ == "__main__" :
     gui.title("Helper")
 
     # Set the configuration of GUI window
-    gui.geometry("400x250")
+    gui.geometry("315x150")
+    gui.resizable(0, 0)
+    gui.rowconfigure(0, weight=1)
+    gui.columnconfigure(0, weight=1)
 
     #StringVar
     res_d_field=StringVar()
@@ -113,35 +148,36 @@ if __name__ == "__main__" :
 
     #RESULTATS
     degat=Label(gui, textvariable= res_d_field, fg="grey")
-    degat_label=Label(gui, text="Dégât basique : ", fg="grey")
+    degat_label=Label(gui, text="Dégât sans défense : ", fg="grey")
 
     res_cuirasse=Label(gui, textvariable=res_cuirasse_field, fg="grey")
     res_cuirasse_label=Label(gui, text="Dégât avec cuirasse : ", fg= "grey")
 
     res_encaissement=Label(gui, textvariable=res_encaissement_field, fg="grey")
-    res_encaissement_label=Label(gui, text="Dégât après encaissement : ", fg="grey")
+    res_encaissement_label=Label(gui, text="Dégât finaux : ", fg="grey")
 
 
     #TITRE
     stats=Label(gui, text="CARACTÉRISTIQUES",fg="maroon")
     dice=Label(gui, text="DÉS", fg="maroon")
     res=Label(gui, text="RÉSULTATS", fg="maroon")
+    vide=Label(gui, text="")
 
 
     #Boutton résultat
-    resultat=Button(gui, text="Résultat",bg ="bisque", fg="maroon", command=calculate_degat)
+    resultat=Button(gui, text="Dégât finaux : ",bg ="bisque", fg="maroon", command=calculate_degat)
 
     #Clear
-    clearEntry=Button(gui,text="Effacer tout",bg ="bisque", fg="maroon", command=clearAll)
+    #clearEntry=Button(gui,text="Effacer tout",bg ="bisque", fg="maroon", command=clearAll)
 
     #Remplissage
-    pv_field=Entry(gui,bg ="bisque", fg="maroon", width="7")
+    pv_field=Spinbox(gui, from_=2, to=1000000, bg ="bisque", fg="maroon", width="7")
     atq_field=Spinbox(gui, from_=0, to=10, width=5,bg ="bisque", fg="maroon")
     defe_field=Spinbox(gui, from_=0, to=10, width=5,bg ="bisque", fg="maroon")
-    shield_field=Entry(gui,bg ="bisque", fg="maroon", width="7")
+    shield_field=Spinbox(gui, from_=0, to=999999, bg ="bisque", fg="maroon", width="7")
     d_endu_field=Spinbox(gui, from_=0, to=10, width=5,bg ="bisque", fg="maroon")
     val_endu_field=Spinbox(gui, from_=0, to=10, width=5,bg ="bisque", fg="maroon")
-    bonus_field=Entry(gui,bg ="bisque", fg="maroon", width="7")
+    bonus_field=Spinbox(gui, from_=0, to=99, bg ="bisque", fg="maroon", width="7")
 
     #res_d_field=Entry(gui)
     #res_cuirasse_field=Entry(gui)
@@ -150,48 +186,48 @@ if __name__ == "__main__" :
 
     #Menu
     #ETAT
-    stats.grid(row=0, column=1)
+    stats.grid(row=0, column=1,sticky="nsew")
 
-    pv.grid(row=1, column=0)
-    pv_field.grid(row=1, column=1)
+    pv.grid(row=1, column=0,sticky="nsew")
+    pv_field.grid(row=1, column=1,sticky="nsew")
 
-    val_endu.grid(row=2, column=0)
-    val_endu_field.grid(row=2, column=1)
+    val_endu.grid(row=2, column=0,sticky="nsew")
+    val_endu_field.grid(row=2, column=1,sticky="nsew")
 
-    shield.grid(row=3, column=0)
-    shield_field.grid(row=3, column=1)
+    shield.grid(row=3, column=0,sticky="nsew")
+    shield_field.grid(row=3, column=1,sticky="nsew")
 
-    bonus.grid(row=4, column=0)
-    bonus_field.grid(row=4, column=1)
+    bonus.grid(row=4, column=0,sticky="nsew")
+    bonus_field.grid(row=4, column=1,sticky="nsew")
 
     #DICES
-    dice.grid(row=0, column=4)
+    dice.grid(row=0, column=3,sticky="nsew")
 
-    atq.grid(row=1, column=3)
-    atq_field.grid(row=1, column=4)
+    atq.grid(row=1, column=2,sticky="nsew")
+    atq_field.grid(row=1, column=3,sticky="nsew")
 
-    d_endu.grid(row=2, column=3)
-    d_endu_field.grid(row=2, column=4)
+    d_endu.grid(row=2, column=2,sticky="nsew")
+    d_endu_field.grid(row=2, column=3,sticky="nsew")
 
-    defe.grid(row=3, column=3)
-    defe_field.grid(row=3, column=4)
+    defe.grid(row=3, column=2,sticky="nsew")
+    defe_field.grid(row=3, column=3,sticky="nsew")
 
     #RESULTAT
+    vide.grid(row=5, column=0,sticky="nsew")
+    resultat.grid(row=6, column=0,pady=1, padx=1)
 
-    resultat.grid(row=5, column=1)
-
-    degat_label.grid(row=6, column=0)
-    degat.grid(row=6, column=1)
+    #degat_label.grid(row=6, column=0)
+    #degat.grid(row=6, column=1)
     #res_d_field.grid(row=6, column=1)
 
-    res_cuirasse_label.grid(row=7, column=0)
-    res_cuirasse.grid(row=7, column=1)
+    #res_cuirasse_label.grid(row=7, column=0)
+    #res_cuirasse.grid(row=7, column=1)
     #res_cuirasse_field.grid(row=7, column=1)
 
-    res_encaissement_label.grid(row=8, column=0)
-    res_encaissement.grid(row=8, column=1)
+    #res_encaissement_label.grid(row=6, column=0)
+    res_encaissement.grid(row=6, column=1)
     #res_encaissement_field.grid(row=8, column=1)
-    clearEntry.grid(row=9, column=1)
+    #clearEntry.grid(row=8, column=1,sticky="nsew")
 
     #start
     gui.mainloop()
