83cf5db7974aaeebefc52adafe2c6e00c936dd41
Mara <mara-li@outlook.fr>
2020-02-12 01:23:30 +0100

    Auto stash before merge of "master" and "origin/master"

--- CDI_dice.py
+++ CDI_dice.py
@@ -39,49 +39,69 @@ def reussite_endurance(endu_de, endu_val, PV,d, SHIELD):
     return finaux
 
 
-def calculate_degat() :
+def capacite_bonus():
+    bonus=int(bonus_field.get())
+    atq = int(atq_field.get())
+    if atq == 0:
+        bonus=bonus*2
+    elif atq == 1:
+        bonus=bonus*1.5
+    else:
+        pass
+    return bonus
+
+def calculate_degat(bonus, ATQ, DEFE):
+    d = abs(ATQ - DEFE)
+    if d == 0 :
+        d = 0 + bonus
+    elif d == 1 :
+        d = 0.05 + bonus
+    elif d == 2 :
+        d = 0.1 + bonus
+    elif d == 4 or d == 3 :
+        d = 0.2 + bonus
+    elif d == 5 or d == 6 :
+        d = 0.3 + bonus
+    elif d == 7 or d == 8 :
+        d = 0.4 + bonus
+    elif d >= 9 :
+        d = 0.5 + bonus
+    return d
+
+def calculate() :
     value = checkError()
     if value == -1 :
         return
     else :
-        d=0
+        selection=int(sel.get())
         PV = int(pv_field.get())
         ATQ = int(atq_field.get())
         DEFE = int(defe_field.get())
         endu_de = int(d_endu_field.get())
         endu_val = int(val_endu_field.get())
         SHIELD = int(shield_field.get()) / 100
-        BONUS = int(bonus_field.get()) / 100
 
+
+        if selection == 2:
+            BONUS=capacite_bonus()/100
+        else:
+            BONUS=int(bonus_field.get())/100
+        d = calculate_degat(BONUS, ATQ, DEFE)
         # Calcul des dégâts
         if ATQ == 0 :  # UltraCC de PJ
             # Un ultra CC outrepasse TOUTES les défense de l'adversaire, Bouclier et défense compris.
             d = 0.5 + BONUS
-        elif DEFE == 0 : #CC de défense : quelque soit l'attaque, elle ne passera pas, sauf en cas de 0/0, où l'attaquant à priorité
-            pass #Permet de sortir de la boucle !
+            endu_val=0
+        elif DEFE == 0 :  # CC de défense : quelque soit l'attaque, elle ne passera pas, sauf en cas de 0/0, où l'attaquant à priorité
+            d=0  # Permet de sortir de la boucle !
         elif ATQ == 1 :  # CC de Mob/Pj normaux
-            # Ici, on outrepasse la défense de l'adversaire, mais on garde tout de même la Bouclier.
-            d = 0.5 + BONUS
-        else :
-            d = abs(ATQ - DEFE)
-            if d == 0 :
-                d = 0 + BONUS
-            elif d == 1 :
-                d = 0.05 + BONUS
-            elif d == 2 :
-                d = 0.1 + BONUS
-            elif d == 4 or d == 3 :
-                d = 0.2 + BONUS
-            elif d == 5 or d == 6 :
-                d = 0.3 + BONUS
-            elif d == 7 or d == 8 :
-                d = 0.4 + BONUS
-            elif d >= 9 :
-                d = 0.5 + BONUS
+            endu_val=0
         finaux=reussite_endurance(endu_de, endu_val, PV, d,SHIELD)
 
         # insert methode : value in the text entry box
         res_finaux_field.set(str(finaux))
+
+
 
 
 # driver code
@@ -92,13 +112,16 @@ if __name__ == "__main__" :
     gui.title("Helper")
 
     # Set the configuration of GUI window
-    gui.geometry("290x140")
+    gui.geometry("380x160")
     gui.resizable(0, 0)
     gui.rowconfigure(0, weight=1)
     gui.columnconfigure(0, weight=1)
 
     # StringVar
     res_finaux_field = StringVar()
+
+    #IntVar
+    sel = IntVar()
 
     # Frames
     cadre_statistique = Frame(gui)
@@ -108,16 +131,20 @@ if __name__ == "__main__" :
     cadre_dice.config(bd=1, relief="groove")
     cadre_dice.grid(row=0, column=3, rowspan=3, columnspan=6, sticky='nsew', ipadx=3)
 
+
     # STATISTIQUES
     pv = Label(cadre_statistique, text="PV")
     shield = Label(cadre_statistique, text="Bouclier")
     val_endu = Label(cadre_statistique, text="Endurance")
     bonus = Label(cadre_statistique, text="Bonus")
 
+
+
     # DICES
     atq = Label(cadre_dice, text="      ATQ")
     defe = Label(cadre_dice, text="      DEF")
     d_endu = Label(cadre_dice, text="      END")
+
 
     # RESULTATS
     helvetica = tkfont.Font(family='Helvetica', size=14)
@@ -128,9 +155,12 @@ if __name__ == "__main__" :
     dice = Label(cadre_dice, text="DÉS", fg="maroon")
 
     # Boutton résultat
-    resultat = Button(gui, text="Dégâts finaux :  ", bg="bisque", fg="maroon", command=calculate_degat, relief=GROOVE,
+    resultat = Button(gui, text="Dégâts finaux :  ", bg="bisque", fg="maroon", command=calculate, relief=GROOVE,
                       takefocus=1, overrelief=GROOVE
                       , width=3)
+
+
+
 
     # Remplissage
     pv_field = Spinbox(cadre_statistique, from_=2, to=1000000, bg="bisque", fg="maroon", width="7")
@@ -140,11 +170,17 @@ if __name__ == "__main__" :
     val_endu_field = Spinbox(cadre_statistique, from_=0, to=10, width=5, bg="bisque", fg="maroon")
     d_endu_field = Spinbox(cadre_dice, from_=0, to=10, width=5, bg="bisque", fg="maroon")
     bonus_field = Spinbox(cadre_statistique, from_=0, to=99, bg="bisque", fg="maroon", width="7")
+        #Button Attaque
+    normal = Radiobutton(cadre_statistique, text="Attaque normale", variable=sel, value=1)
+    capa = Radiobutton(cadre_statistique, text="Capacité", variable=sel, value=2)
+        #Valeur par défaut
+    normal.select()
+
 
     # Menu
     # ETAT
 
-    stats.grid(row=0, column=1, sticky="nsew")
+    stats.grid(row=0, column=0, sticky="nsew", columnspan=3, padx=100)
     pv.grid(row=1, column=0, sticky="nsew")
     pv_field.grid(row=1, column=1, sticky="nsew")
     shield.grid(row=2, column=0, sticky="nsew")
@@ -153,6 +189,10 @@ if __name__ == "__main__" :
     val_endu_field.grid(row=3, column=1, sticky="nsew")
     bonus.grid(row=4, column=0, sticky="nsew")
     bonus_field.grid(row=4, column=1, sticky="nsew")
+        #button
+    normal.grid(row=5, column=0, sticky='nsew')
+    capa.grid(row=5, column=1, sticky='nsew')
+
 
     # DICES
     dice.grid(row=0, column=3, sticky="nsew")
@@ -164,7 +204,7 @@ if __name__ == "__main__" :
     defe_field.grid(row=2, column=3, sticky="nsew")
 
     # RESULTAT
-    resultat.grid(row=6, column=0, ipadx=100, sticky='ns')
+    resultat.grid(row=6, column=0, ipadx=100000, sticky='ns')
     res_finaux.grid(row=6, column=5, columnspan=100, sticky='s')
 
     # start
