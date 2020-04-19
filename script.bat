1df04e791451d00b528e9e2bb5cf3d3271805d0f
Lisandra <mara-li@outlook.fr>
2020-04-19 20:38:04 +0200

    edit

--- script.bat
+++ script.bat
@@ -1,3 +1,3 @@
 @echo off
-pyinstaller -y -w -F -i "data/logo.ico" --add-data "data/reset.png";"data" --add-data "data/log_file.png";"data" "CDI_dice.py"
-pyinstaller -y -w -i "data/logo.ico" --add-data "data/reset.png";"data" --add-data "data/log_file.png";"data" "CDI_dice.py"
+pyinstaller -y -w -F -i "data/logo.ico" --add-data "data/reset.png";"data" --add-data "data/log_file.png";"data" "JDR_System.py"
+pyinstaller -y -w -i "data/logo.ico" --add-data "data/reset.png";"data" --add-data "data/log_file.png";"data" "JDR_System.py"
