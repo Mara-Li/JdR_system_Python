888469c3e94d959b762d3bfcfa3196c9929647af
Lisandra <mara-li@outlook.fr>
2020-04-27 12:34:54 +0200

    nettoyage => Master : Page html

--- script.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-pyinstaller -y -w -F -i "data/logo.ico" --add-data "data/reset.png";"data" --add-data "data/log_file.png";"data" "JDR_System.py"
-pyinstaller -y -w -i "data/logo.ico" --add-data "data/reset.png";"data" --add-data "data/log_file.png";"data" "JDR_System.py"
